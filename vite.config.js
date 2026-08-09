27fd6ef6e673b19e74f5056850107a44629fccbc
Syauqi Nuzul Abdi <nuzulabdisyauqi@gmail.com>
2026-08-10 02:12:11 +0800

    chore(build): add Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vites';
 import laravel from 'laravel-vite-plugin';
 import { bunny } from 'laravel-vite-plugin/fonts';
 import tailwindcss from '@tailwindcss/vite';
